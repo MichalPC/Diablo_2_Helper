6b5cb906c135988cec123f868f81869fd5a3c389
MichalPC <zfac211@rhul.ac.uk>
2020-08-31 17:03:55 +0100

    Remove unused imports from ExampleInstrumentedTest.java.

--- app/src/androidTest/java/com/example/diablo2runewords/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/diablo2runewords/ExampleInstrumentedTest.java
@@ -4,7 +4,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
