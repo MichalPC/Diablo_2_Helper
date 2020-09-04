e6f5213cecd0e232e7921fc3d15653aa4469c90c
MichalPC <zfac211@rhul.ac.uk>
2020-09-04 18:13:44 +0100

    Create a View for SplashScreen.java

--- app/src/main/java/com/example/diablo2runewords/SplashScreen.java
+++ app/src/main/java/com/example/diablo2runewords/SplashScreen.java
@@ -3,12 +3,25 @@ package com.example.diablo2runewords;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
 
 public class SplashScreen extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_splash_screen);
+
+    View easySplashScreenView = new EasySplashScreen(SplashScreen.this)
+        .withFullScreen()
+        .withTargetActivity(MainActivity.class)
+        .withSplashTimeOut(4000)
+        .withBackgroundResource(android.R.color.holo_red_light)
+        .withLogo(R.drawable.fav_rune_word_icon)
+        .withAfterLogoText("D2 Helper")
+        .create();
+
+    setContentView(easySplashScreenView);
   }
 }
